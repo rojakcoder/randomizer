420c6b8a49e9f385c9ef0d54d18ca72de716d045
CH Chua <chuacheehow@gmail.com>
2019-01-24 20:00:33 +0800

    Added configuration to generate minified version of app for production.
    - Added the Babel configuration file as well.

---
+++ scripts/rollup.config.prod.js
@@ -0,0 +1,51 @@
+import babel from 'rollup-plugin-babel';
+import vue from 'rollup-plugin-vue';
+import css from 'rollup-plugin-css-only';
+import resolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import postcss from 'rollup-plugin-postcss'; // This must be imported in order to compile SCSS.
+import scss from 'rollup-plugin-scss';
+import { uglify } from 'rollup-plugin-uglify';
+
+var plugins = [
+  vue({
+    css: false
+  }),
+  babel({
+    runtimeHelpers: true,
+    sourceMap: true,
+    extensions: ['.js', '.vue']
+  }),
+  css(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }),
+  resolve(),
+  //uglify()
+];
+
+export default [
+  {
+    input: 'src/main.js',
+    output: {
+      file: 'dist/build.min.js',
+      format: 'iife'
+    },
+    plugins
+  },
+  {
+    input: 'sass/main.js',
+    output: {
+      file: 'dist/main.js',
+      format: 'iife'
+    },
+    plugins: [
+      scss({
+        outputStyle: 'compressed',
+        output: 'dist/main.css'
+      })
+    ]
+  }
+];
+
+
